1db4e0d2a011b277ac4f7b74ae30ae83a250ae30
Marsdirt <marsdirt@gmail.com>
2025-06-15 17:55:49 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,8 @@
 
 import React, { useState, useEffect } from 'react';
 import { Header } from '@/components/Header';
-// import { Footer } from '@/components/Footer'; // Original alias import
-import { Footer } from './src/components/Footer.tsx'; // Corrected relative import for Footer
-// import { FilterPanel } from '../components/FilterPanel.tsx'; // Original failing import, now commented
+// import { FilterPanel } from '../components/FilterPanel.tsx'; // Temporarily removed for testing
+import { Footer } from '../components/Footer.tsx'; // Corrected relative import for Footer test
 import { ListingsGrid } from '@/components/ListingsGrid';
 import { LoadingSpinner } from '@/components/LoadingSpinner';
 import { NoResults } from '@/components/NoResults';
@@ -88,15 +87,15 @@ export default function HomePage() {
       <main className="flex-grow container mx-auto px-4 py-8">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
           <div className="lg:col-span-4 xl:col-span-3">
-            {/* 
+            {/*
             <FilterPanel
               filters={filters}
               onFilterChange={handleFilterChange}
               onSearch={() => handleSearch()}
               isSearching={isLoading}
-            /> 
+            />
             */}
-            <div>FilterPanel is commented out for testing.</div>
+            <div>FilterPanel is commented out for testing. Footer component should appear below.</div>
           </div>
           <div className="lg:col-span-8 xl:col-span-9">
             {isLoading ? (
@@ -111,7 +110,7 @@ export default function HomePage() {
           </div>
         </div>
       </main>
-      <Footer /> {/* This should work if the import works */}
+      <Footer /> {/* Using the imported Footer component */}
     </div>
   );
 }
