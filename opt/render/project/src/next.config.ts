eff3c03fd623145a7fb74713ab292c7d8424e7d3
Marsdirt <marsdirt@gmail.com>
2025-06-16 20:43:46 -0700

    Update next.config.ts

--- opt/render/project/src/next.config.ts
+++ opt/render/project/src/next.config.ts
@@ -3,7 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone', // Recommended for Docker/serverless environments
-  srcDir: 'src', // Explicitly set the source directory
   images: {
     remotePatterns: [
       {
